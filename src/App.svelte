2a0fff372e634f989407dd50d41449e806528903
erlanggawulung <erlanggawulung@gmail.com>
2022-04-15 14:26:18 +0700

    install jquery

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,27 @@
 <script>
 	import Preloader from './Preloader.svelte'
 	export let name
+	
 </script>
 
 <main>
-	<Preloader />
-	<section class="container-fluid main-container container-home">
+		<Preloader />
+		<section class="container-fluid main-container container-home">
     <div class="color-block d-none d-lg-block"></div>
     <div class="row home-details-container align-items-center">
-        <div class="col-lg-4 bg position-fixed d-none d-lg-block"></div>
-        <div class="col-12 col-lg-8 offset-lg-4 home-details text-left text-sm-center text-lg-left">
-            <div>
-								<!-- svelte-ignore a11y-img-redundant-alt -->
-								<img src="image-3-square.jpeg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="profile picture" />	
-                <h1 class="text-uppercase poppins-font">I'm {name}.<span>software dev</span></h1>
-                <p class="open-sans-font">I'm an Indonesian based software developer focused on building back-end side. I am passionate about building excellent software that improves the lives of those around me.</p>
-								<a class="button" href="/">
-									<span class="button-text">more about me</span>
-									<span class="button-icon fa fa-arrow-right"></span>
-								</a>
-            </div>
-        </div>
+			<div class="col-lg-4 bg position-fixed d-none d-lg-block"></div>
+				<div class="col-12 col-lg-8 offset-lg-4 home-details text-left text-sm-center text-lg-left">
+					<div>
+						<!-- svelte-ignore a11y-img-redundant-alt -->
+						<img src="image-3-square.jpeg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="profile picture" />	
+						<h1 class="text-uppercase poppins-font">I'm {name}.<span>software dev</span></h1>
+						<p class="open-sans-font">I'm an Indonesian based software developer focused on building back-end side. I am passionate about building excellent software that improves the lives of those around me.</p>
+						<a class="button" href="/">
+							<span class="button-text">more about me</span>
+							<span class="button-icon fa fa-arrow-right"></span>
+						</a>
+					</div>
+			</div>
     </div>
 	</section>
 </main>
@@ -39,4 +40,5 @@
 		top: 40px;
 		box-shadow: 0 0 7px rgba(0,0,0,.9);
 	}
+
 </style>
