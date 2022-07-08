441faee9826600242a23511c23acb24c18703f6c
Wulung <erlanggawulung@gmail.com>
2022-07-08 12:48:49 +0700

    update landing page, add social media links

--- src/App.svelte
+++ src/App.svelte
@@ -15,11 +15,10 @@
 						<!-- svelte-ignore a11y-img-redundant-alt -->
 						<img src="image-3-square.jpeg" class="img-fluid main-img-mobile d-none d-sm-block d-lg-none" alt="profile picture" />	
 						<h1 class="text-uppercase poppins-font">I'm {name}.<span>software dev</span></h1>
-						<p class="open-sans-font">I'm an Indonesian based software developer focused on building back-end side. I am passionate about building excellent software that improves the lives of those around me.</p>
-						<a class="button" href="/">
-							<span class="button-text">more about me</span>
-							<span class="button-icon fa fa-arrow-right"></span>
-						</a>
+						<p class="open-sans-font">Hello, I'm Wulung an Indonesian based software developer focused on building back-end side. I am passionate about building excellent software that improves the lives of those around me.</p>
+					</div>
+					<div>
+						<p class="open-sans-font">Find me on <b><a href="https://www.linkedin.com/in/erlangga-wulung/" target="_blank">LinkedIn</a><b>, <b><a href="https://github.com/erlanggawulung" target="_blank">GitHub</a></b>, and <b><a href="https://gitlab.com/erlanggawulung" target="_blank">GitLab</b>.</p>
 					</div>
 			</div>
     </div>
@@ -39,6 +38,9 @@
 		left: 40px;
 		top: 40px;
 		box-shadow: 0 0 7px rgba(0,0,0,.9);
+	}
+	a {
+		color: #ff9100e1
 	}
 
 </style>
